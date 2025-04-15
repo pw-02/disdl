cc28b8227005f4558f55a48a62e0a94c88777dbd
pw-02 <watters.patrick@outlook.com>
2025-04-14 23:18:50 -0700

    regnet_y_400mf

--- workloads/scripts/run_imagenet_hpo.py
+++ workloads/scripts/run_imagenet_hpo.py
@@ -26,7 +26,7 @@ def main(config: DictConfig):
     workload = "imagenet_hpo"
     dataloader = "coordl" #tensorsocket, disdl, coordl
     producer_only = False
-    model = "resnet18" #resnet18, regnet_y_400mf,imagenet_mobilenet_v3_large, resnet50, shufflenet_v2_x1_0, vgg16
+    model = "regnet_y_400mf" #resnet18, regnet_y_400mf,imagenet_mobilenet_v3_large, resnet50, shufflenet_v2_x1_0, vgg16
     models = [model,model,model,model]
     learning_rates = [0.1, 0.01, 0.001, 0.0001]  # Add your learning rates here
 

33fe68cd01ca9f6481d4a6327e8a98581a088e91
pw-02 <watters.patrick@outlook.com>
2025-04-28 15:14:44 -0700

    corodl sim almost working

--- simulation/sim_coordl.py
+++ simulation/sim_coordl.py
@@ -4,51 +4,15 @@ import logging
 from typing import List, Tuple, Dict, Set, Any
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
-
+import sys
+sys.path.append(".")
+from simulation.workloads import workloads
+import os
+import csv
+import time
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-class RedisCache:
-    def __init__(self, 
-                 max_cache_size_gb,
-                 size_per_batch_gb, 
-                 num_jobs, 
-                 eviction_policy="LRU"):
-        
-        self.data:Dict[Any,Set] = {}  # Stores cached batches
-        self.size_per_batch_gb = size_per_batch_gb
-        self.num_jobs = num_jobs
-        self.max_cache_size_gb = max_cache_size_gb
-        self.eviction_policy = eviction_policy
-        self.num_jobs = num_jobs
-        self.batch_ref_count = {} #counter for the number of times a batch has been accessed
-        self.cache_size_over_time = []
-
-    def handle_request(self, key, job_id):
-        cache_hit = False
-        if key in self.data:
-            logger.debug(f"Cache hit: Job {job_id} accessed {key}")
-            self.batch_ref_count[key] = self.batch_ref_count.get(key, 0) + 1
-            if self.batch_ref_count[key] >= self.num_jobs:
-                self.data.pop(key, None)                 #evict the batch from the cache
-            cache_hit = True
-        else:
-            # Check if the cache is full and apply eviction policy
-            if (len(self.data) + 1) * self.size_per_batch_gb >= self.max_cache_size_gb:
-                    pass
-                # if self.eviction_policy == "LRU":
-            else:
-                # Add the new batch to the cache
-                self.data[key] = set()
-                logger.debug(f"Cache miss: Job {job_id} added {key} to cache")
-                self.data[key].add(job_id)
-
-                #Check if its the first time the batch is added to the cache
-                if self.batch_ref_count.get(key, 0) == 0:
-                    cache_hit = True
-                self.batch_ref_count[key] = self.batch_ref_count.get(key, 0) + 1 #cache get(key, 1) for now to assume prefetching, but it should be 0
-        self.cache_size_over_time.append(len(self.data))
-        return cache_hit
 
 def calculate_elasticache_serverless_cost(
     average_gb_usage: float,
@@ -60,10 +24,131 @@ def calculate_elasticache_serverless_cost(
     storage_cost = gb_hours * price_per_gb_hour
     total_cost = storage_cost + ecpu_cost
     return total_cost
-   
+
+def save_dict_list_to_csv(dict_list, output_file):
+    if not dict_list:
+        print("No data to save.")
+        return
+    headers = dict_list[0].keys()
+    file_exists = os.path.isfile(output_file)
+    with open(output_file, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers, delimiter=',')
+        if not file_exists:
+            writer.writeheader()
+        for data in dict_list:
+            writer.writerow(data)
+
+class CoorDLCache:
+    def __init__(self, 
+                 cache_capacity_gb,
+                 size_per_batch_gb, 
+                 num_jobs, 
+                 eviction_policy="uniform"):
+        
+        self.data:Dict[Any,int] = {}  # Stores cached batches, and reference counts or the number of times a batch has been accessed
+        self.size_per_batch_gb = size_per_batch_gb
+        self.num_jobs = num_jobs
+        self.cache_capacity_gb = cache_capacity_gb
+        self.eviction_policy = eviction_policy
+        self.num_jobs = num_jobs
+        self.cache_size_over_time = []
+    
+    def get(self, key, job_id):
+        # Simulate cache access and return the batch if it exists
+        if key in self.data:
+            logger.debug(f"Cache hit: Job {job_id} accessed {key}")
+            self.data[key] += 1 # Increment the reference count for the batch
+            # Check if the batch has been accessed by all jobs, and evict if cache policy is uniform
+            if self.data[key] >= self.num_jobs and self.eviction_policy == "uniform":
+                logger.debug(f"Evicting batch {key} from cache")
+                self.data.pop(key, None)
+            return True
+        else:
+            logger.debug(f"Cache miss: Job {job_id} could not access {key}")
+            # Check if the cache is full and apply eviction policy if necessary
+            if self.cache_is_full() and self.eviction_policy not in ["None", "uniform"]:
+                self.run_evition_policy()
+            #Add the new batch to the cache
+            if not self.cache_is_full():
+                self.data[key] = 1
+            
+            return False
+    
+    def cache_is_full(self):
+        # Check if the cache is full based on the size of the batches and the maximum cache size
+        return (len(self.data) + 1) * self.size_per_batch_gb >= self.cache_capacity_gb
+    
+    def run_evition_policy(self):
+        # Implement the eviction policy here (e.g., LRU, LFU, etc.)
+        if self.eviction_policy == "LRU":
+            # Find the least recently used batch and evict it
+            lru_key = min(self.data, key=self.data.get)
+            logger.debug(f"Evicting batch {lru_key} from cache (LRU)")
+            self.data.pop(lru_key, None)
+    
+    def get_cache_size(self):
+        # Calculate the current cache size in GB
+        return len(self.data) * self.size_per_batch_gb
+    
+    # def get_cache_length(self):
+    #     # Calculate the current cache size in GB
+    #     return len(self.data) * self.size_per_batch_gb
+
+class DLTJOB():
+    def __init__(self, job_id, speed, cache:CoorDLCache):
+        self.job_id = job_id
+        self.cache = cache  # Reference to the shared cache
+        self.speed = speed  # Speed in GPU time per batch (seconds)
+        self.job_progress = 0  # Tracks how many batches the job has completed
+        self.cache_hit_count = 0  # Tracks cache hits for this job
+        self.cache_miss_count = 0  # Tracks cache misses for this job
+        self.elapased_time_sec = 0  # Tracks the current time for this job
+
+    def next_training_step(self, current_time_sec):
+        self.elapased_time_sec = current_time_sec        
+        cache_hit = self.cache.get(self.job_id, self.job_id)  # Simulate cache access
+        if cache_hit:
+            self.cache_hit_count +=1
+        else:
+            self.cache_miss_count += 1
+        self.job_progress += 1
+        return cache_hit
+
+    
+    def get_job_progress(self, ):
+        return self.job_progress
+
+    def __lt__(self, other):
+        return self.speed < other.speed  # Compare based on speed
+    
+    def get_performance(self, run_id, ec2_cost=12.24, cache_cost=3.25):
+        # Calculate performance metrics for this job
+        cache_hit_rate = self.cache_hit_count / (self.cache_hit_count + self.cache_miss_count) if (self.cache_hit_count + self.cache_miss_count) > 0 else 0
+        throughput = self.job_progress / self.elapased_time_sec if self.elapased_time_sec > 0 else 0
+        # Calculate costs
+        compute_cost = (ec2_cost / 3600) * self.elapased_time_sec
+        cache_cost = (cache_cost / 3600) * self.elapased_time_sec
+        total_cost = compute_cost + cache_cost
+        return {
+            'run_id': run_id,
+            'job_id': self.job_id,
+            'job_speed': self.speed,
+            'cache_capacity_gb': self.cache.cache_capacity_gb,
+            'cache_hit_%': cache_hit_rate,
+            'bacthes_processed': self.job_progress,
+            # 'cache_hit_count': self.cache_hit_count,
+            # 'cache_miss_count': self.cache_miss_count,
+            'elapsed_time': self.elapased_time_sec,
+            'throughput': throughput,
+            'compute_cost': compute_cost,
+            'cache_cost': cache_cost,
+            'total_cost': total_cost,
+        }
+
 def run_coordl_simulation(
-        job_speeds,
-        max_cache_size_gb,
+        workload_name,
+        workload_jobs,
+        cache_capacity_gb,
         size_per_batch_gb,
         cache_miss_penalty,
         hourly_cache_cost,
@@ -72,66 +157,76 @@ def run_coordl_simulation(
         batches_per_job=np.inf,
         use_elasticache_severless_pricing=False):
     
-    num_jobs = len(job_speeds)
-    job_progress = [0] * num_jobs  # Tracks how many batches each job has completed
-    event_queue = []  # Priority queue for next event times
-    cache_miss_count = 0
-    cache_hit_count = 0
+    #rando id based in current datetime
+    run_id = str(int(time.time()))
     
-    cache = RedisCache(
-        max_cache_size_gb = max_cache_size_gb,
-        size_per_batch_gb=size_per_batch_gb,
-        num_jobs = num_jobs
-    )
-   
-    for job_id, speed in enumerate(job_speeds):
-        heapq.heappush(event_queue, (speed, job_id))  # (time to complete first batch, job_id)
-
+    shared_cache = CoorDLCache(cache_capacity_gb = cache_capacity_gb,
+                       size_per_batch_gb=size_per_batch_gb,
+                       num_jobs = len(workload_jobs))
+    
+    jobs:List[DLTJOB] = [DLTJOB(model_name, speed, shared_cache) for model_name, speed in workload_jobs]
+    cache_size_over_time = []  # Store cache size over time for plotting
+    
+    #set tying for evenet queue
+    event_queue = []  # Priority queue for next event times
+    for job in jobs:
+        heapq.heappush(event_queue, (job.speed, job))
+    
     time_elapsed = 0  # Global simulation time
 
     while True:
         if simulation_time_sec is not None and time_elapsed >= simulation_time_sec:
             break
         #check all jobs have completed their batches
-        if batches_per_job is not None and all(job_progress[job_id] >= batches_per_job for job_id in range(num_jobs)):
+        if batches_per_job is not None and all(job.job_progress >= batches_per_job for job in jobs):
             break
-        
-        time_elapsed, job_id = heapq.heappop(event_queue)  # Get next job event
-        current_batch = job_progress[job_id]  # Current batch for the job
-        cache_hit = cache.handle_request(current_batch, job_id)  # Handle cache request
-        if cache_hit:
-            cache_hit_count +=1
-            next_event_time = time_elapsed + (job_speeds[job_id])
-        else:
-            cache_miss_count += 1
-            next_event_time = time_elapsed + (job_speeds[job_id]) + cache_miss_penalty
-        
-        job_progress[job_id] += 1
-        if batches_per_job is not None and job_progress[job_id] < batches_per_job:
-            heapq.heappush(event_queue, (next_event_time, job_id))
-        else:
-            heapq.heappush(event_queue, (next_event_time, job_id))
 
+        time_elapsed, job = heapq.heappop(event_queue)
+        job:DLTJOB = job  # Get next job event
+        cache_hit = job.next_training_step(time_elapsed)  # Simulate the next training step for this job
+        #schudle the next event for this job if it has not completed its batches
+        if batches_per_job is not None and job.job_progress < batches_per_job:
+            if cache_hit:
+                next_event_time = time_elapsed + job.speed
+            else:
+                next_event_time = time_elapsed + job.speed + cache_miss_penalty
+            heapq.heappush(event_queue, (next_event_time, job))
+        cache_size_over_time.append(shared_cache.get_cache_size())  # Store cache size over time
     
-    total_batches_processed = sum(job_progress)
+    # Calculate results
+    total_batches_processed = sum(job.job_progress for job in jobs)
     throughput = total_batches_processed / time_elapsed  # Batches per second
     compute_cost = (hourly_ec2_cost / 3600) * time_elapsed
-
-
-    # cache_cost = hourly_cache_cost
+    cache_hit_count = sum(job.cache_hit_count for job in jobs)
+    cache_miss_count = sum(job.cache_miss_count for job in jobs)
     cache_hit_percent = (cache_hit_count / (cache_hit_count + cache_miss_count)) * 100 if (cache_hit_count + cache_miss_count) > 0 else 0
-    max_cached_bacthes = max(cache.cache_size_over_time)
-    max_cache_capacity_used = max_cached_bacthes * size_per_batch_gb
+    # max_cached_bacthes = max(shared_cache.cache_size_over_time)
+    # max_cache_capacity_used = max_cached_bacthes * size_per_batch_gb
+    max_cache_capacity_used = max(cache_size_over_time) if cache_size_over_time else 0
+    average_cache_capacity_used = np.mean(cache_size_over_time) if cache_size_over_time else 0
+
     if use_elasticache_severless_pricing:
-        cache_cost = calculate_elasticache_serverless_cost(average_gb_usage=max_cache_capacity_used)
+        cache_cost = calculate_elasticache_serverless_cost(average_gb_usage=average_cache_capacity_used)
     else:
         cache_cost = (hourly_cache_cost / 3600) * time_elapsed
     total_cost = compute_cost + cache_cost  # No additional costs in this simulation
 
-    #resote these results in a dict to be used in the report
-    results = {
-        'cache_miss_count': cache_miss_count,
+    overall_results = {
+        'run_id': run_id,
+        'workload_name': workload_name,
+        'job_speeds': {job.job_id: job.speed for job in jobs},
+        'dataloader': 'CoorDL',
+        'cache_capacity': cache_capacity_gb,
+        'cache_eviction_policy': shared_cache.eviction_policy,
+        'size_per_batch': size_per_batch_gb,
+        'num_jobs': len(jobs),
+        'cache_miss_penalty': cache_miss_penalty,
+        'hourly_ec2_cost': hourly_ec2_cost,
+        'hourly_cache_cost': hourly_cache_cost,
+        'max_cache_capacity_used': max_cache_capacity_used,
+        'average_cache_capacity_used': average_cache_capacity_used,
         'cache_hit_count': cache_hit_count,
+        'cache_miss_count': cache_miss_count,
         'cache_hit_percent': cache_hit_percent,
         'total_batches_processed': total_batches_processed,
         'time_elapsed': time_elapsed,
@@ -139,19 +234,25 @@ def run_coordl_simulation(
         'compute_cost': compute_cost,
         'cache_cost': cache_cost,
         'total_cost': total_cost,
-        'max_cache_capacity_used_gb': max_cache_capacity_used,
-        'max_cached_bacthes': max_cached_bacthes
     }
+    #save overall results to a file
+    report_folder = os.path.join(os.getcwd(), "simulation", "reports", workload_name)
+    os.makedirs(report_folder, exist_ok=True)
     
-    #print some results
+    report_file = os.path.join(report_folder, "overall_results.csv")
+    save_dict_list_to_csv([overall_results], report_file)
+
+    job_performances = [job.get_performance(run_id, hourly_ec2_cost, hourly_cache_cost) for job in jobs]
+    job_performance_file = os.path.join(report_folder, "job_results.csv")
+    save_dict_list_to_csv(job_performances, job_performance_file)
+
+    #save results to a file
+
     print(f"CoorDL:")
     print(f"  Time: {time_elapsed:.2f} seconds")
-    print(f"  Cache Size: {max_cache_size_gb} GB")
+    print(f"  Cache Size: {cache_capacity_gb} GB")
     print(f"  Cache Used: {max_cache_capacity_used:.4f} GB")
-    # print(f"  Max Cached Batches: {max_cached_bacthes} GB")
-    print(f"  Cache Miss Count: {cache_miss_count}")
-    print(f"  Cache Hit Count: {cache_hit_count}") 
-    print(f"  Cache Hit Percentage: {cache_hit_percent:.2f}%")
+    print(f"  Cache Hit %: {cache_hit_percent:.2f}%")
     print(f"  Total Batches Processed: {total_batches_processed}")
     print(f"  Elapsed Time: {time_elapsed:.2f}s, {time_elapsed/60:.2f} min")
     print(f"  Overall Throughput: {throughput:.2f} batches/sec")
@@ -159,23 +260,26 @@ def run_coordl_simulation(
     print(f"  Compute Cost: ${compute_cost:.2f}")
     print(f"  Total Cost : ${total_cost:.2f}")
     print("-" * 40)
-    return results
+    return overall_results
    
 if __name__ == "__main__":
-    from simulation.models_v100_times import RESNET18, RESNET50, SHUFFLENETV, VGG16
-    models = [RESNET18, RESNET50, SHUFFLENETV, VGG16]
+
+    #print name variable name 'imagenet_128_batch_size'
+    workload_name = 'imagenet_128'
+    workload =  workloads[workload_name]
     simulation_time_sec =  None #3600 * 5 # Simulate 1 hour
     max_batches_per_job = 8500 #np.inf
     hourly_ec2_cost = 12.24 
     hourly_cache_cost = 3.25
-    redis_cache_size_gb = 150 #np.inf
+    cache_capacity_gb = 100 #np.inf
     size_per_batch_gb = 20 / 1024
-    cache_miss_penalty = 0
+    cache_miss_penalty = 0.5
     use_elasticache_severless_pricing = False
 
     run_coordl_simulation(
-        job_speeds = models,
-        max_cache_size_gb=redis_cache_size_gb,
+        workload_name = workload_name,
+        workload_jobs = workload.items(),
+        cache_capacity_gb=cache_capacity_gb,
         size_per_batch_gb = size_per_batch_gb,
         cache_miss_penalty = cache_miss_penalty,
         hourly_cache_cost = hourly_cache_cost,
---
+++ simulation/workloads.py
@@ -0,0 +1,21 @@
+#based on batch size of 128 and imagenet dataset
+
+#imagenet dataset
+#batch size of 128
+
+workloads = {
+    'imagenet_128': {
+        'RESNET18': 0.104419951,
+        'RESNET50': 0.33947309,
+        'VGG16': 0.514980298,
+        'SHUFFLENETV2': 0.062516984
+    },
+}
+
+
+# imagenet_128_batch_size = {
+#     'RESNET18': 0.104419951,
+#     'RESNET50': 0.33947309,
+#     'VGG16': 0.514980298,
+#     'SHUFFLENETV2': 0.062516984
+# }
